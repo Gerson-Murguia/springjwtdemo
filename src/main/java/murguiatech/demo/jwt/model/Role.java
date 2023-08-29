7e3a3b40963082c90f5a85ce848fb1195799c599
GersonMurguia <aldmurguia1@gmail.com>
2023-08-29 00:09:02 -0500

    Creado usuario para autenticacion

---
+++ src/main/java/murguiatech/demo/jwt/model/Role.java
@@ -0,0 +1,6 @@
+package murguiatech.demo.jwt.model;
+
+public enum Role {
+    ADMIN,
+    USER
+}
---
+++ src/main/java/murguiatech/demo/jwt/model/User.java
@@ -0,0 +1,67 @@
+package murguiatech.demo.jwt.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Accessors(fluent = true, chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false,unique = true)
+    private String username;
+    private String lastName;
+    private String firstName;
+    private String country;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    //all true since jwt will provide this
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
