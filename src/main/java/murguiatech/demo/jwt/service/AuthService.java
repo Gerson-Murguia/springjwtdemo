55603ce1aff4baedddc650731fc7578664c9c04a
GersonMurguia <aldmurguia1@gmail.com>
2023-08-29 20:33:32 -0500

    Terminada funcionalidad de login y registro de usuario

--- src/main/java/murguiatech/demo/jwt/service/AuthService.java
+++ src/main/java/murguiatech/demo/jwt/service/AuthService.java
@@ -1,8 +1,13 @@
 package murguiatech.demo.jwt.service;
 
 import lombok.RequiredArgsConstructor;
+import murguiatech.demo.jwt.jwt.JwtService;
 import murguiatech.demo.jwt.model.*;
 import murguiatech.demo.jwt.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,14 +16,24 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
     public AuthResponseToken login(LoginRequest request) {
-        return null;
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                request.getUsername(),request.getPassword()));
+        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(user);
+
+        return AuthResponseToken.builder()
+                .token(token)
+                .build();
     }
 
     public AuthResponseToken register(RegisterRequest request) {
         User user = new User()
                 .username(request.getUsername())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .country(request.getCountry())
